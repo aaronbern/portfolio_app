e2508440aee01d7785d74e253db7bc1c1c95521e
Aaron Bernard <aaron_bernard@trimble.com>
2025-12-30 17:16:03 -0800

    Changing styling on stars/about page

--- src/lib/ProjectsData.tsx
+++ src/lib/ProjectsData.tsx
@@ -17,28 +17,27 @@ export const projects: Project[] = [
     id: 1,
     title: 'Trajectory Oracle',
     description: 'Predicts object trajectories with reinforcement learning.',
-    image: '/traj.jpg', // Image for the project card
-    videoPath: "/videos/output_with_trajectory.mp4", // Video for detailed view
-    detailedImage: "/prediction.png", // Additional image for detailed view
-    githubLink: "https://github.com/aaronbern/Trajectory-Oracle"
+    image: '/traj.jpg',
+    videoPath: '/videos/output_with_trajectory.mp4',
+    detailedImage: '/prediction.png',
+    githubLink: 'https://github.com/aaronbern/Trajectory-Oracle'
   },
   {
     id: 2,
     title: 'Yap-Chat',
     description: 'A real-time web chat app built with the MERN stack, featuring live updates and scalable architecture.',
-    link: 'https://yapp-chat-app-de1a44a0cf7e.herokuapp.com/',  // Link to live app
-
-    image: '/chat.jpg', // Image for the project card
-    detailedImage: "/selected_yap-chat.png", // Detailed image for the expanded view
-    githubLink: "https://github.com/aaronbern/yap"
+    link: 'https://yapp-chat-app-de1a44a0cf7e.herokuapp.com/',
+    image: '/chat.jpg',
+    detailedImage: '/selected_yap-chat.png',
+    githubLink: 'https://github.com/aaronbern/yap'
   },
   {
     id: 3,
-    title: "Vulkan Game Engine",
-    description: "A Vulkan-powered game engine.",
-    example: "Rendering a simple triangle.", // Example or additional info
-    image: "/engine.jpg", // Image for the project card
-    videoPath: "/videos/enginevid.mp4",
-    githubLink: "https://github.com/aaronbern/VulkanGameEngine"
+    title: 'Vulkan Game Engine',
+    description: 'A Vulkan-powered game engine focused on low-level rendering control and performance.',
+    example: 'Rendering a simple triangle.',
+    image: '/engine.jpg',
+    videoPath: '/videos/enginevid.mp4',
+    githubLink: 'https://github.com/aaronbern/VulkanGameEngine'
   }
 ];
